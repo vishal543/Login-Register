77ac199bb25c4afd3977f2ca65b4949fd5afb7f4
vishal <vishal.kariveda543@gmail.com>
2019-01-29 22:10:22 +0530

    initial commit

--- models/user.js
+++ models/user.js
@@ -26,7 +26,7 @@ const User = module.exports = mongoose.model('User', UserSchema);
 module.exports.getUserById = function(id, callback){
   User.findById(id, callback);
 }
-
+ 
 module.exports.getUserByUsername = function(username, callback){
   const query = {username: username}
   User.findOne(query, callback);
